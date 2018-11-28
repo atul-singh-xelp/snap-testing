aa2c6e84c9add3088a9a1f36e454b6ee116c50f3
Atul Singh <30830967+atul-singh-xelp@users.noreply.github.com>
2018-11-28 16:27:24 +0530

    Add files via upload

---
+++ entryfile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+// accept a name for example and a domNode where to render
+function renderIt(name, domNode) {
+  render(<Hello name={name} />, domNode);
+}
+window.renderIt = renderIt;
